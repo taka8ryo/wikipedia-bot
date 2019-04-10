4dbb356fbeda14c893074c9a47dae028b7db3c28
ryo <0823.tr@gmail.com>
2019-04-10 18:28:57 +0900

    fix linebot_contrlller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,6 @@
 class LinebotController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
+  require 'line/bot'
+  require 'wikipedia'
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
@@ -22,13 +23,30 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each { |event|
+      if event.message['text'] != nil
+        word = event.message['text']
+        # 日本語版Wikipediaを利用する
+        Wikipedia.Configure {
+          domain 'ja.wikipedia.org'
+          path   'w/api.php'
+        }
+      end
+
+      # 記事を取得
+      page = Wikipedia.find(word)
+
+      # サマリーを標準出力
+      response = page.summary + "\n" + page.fullurl
+
       case event
+      # メッセージが送信された場合
       when Line::Bot::Event::Message
         case event.type
+        # メッセージが送られて来た場合
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: response
           }
           client.reply_message(event['replyToken'], message)
         end
